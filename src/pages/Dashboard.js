023a513c64e027c826c9aac0ac66a37077b71ea5
anggiie <anggiie02>
2022-09-18 23:36:29 +0200

    add start project tinder clone, add 3 pages

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,6 @@
+const Dashboard = () => {
+    return (
+        <div>Welcome Dashboard</div>
+    )
+}
+export default Dashboard
---
+++ src/pages/OnBoarding.js
@@ -0,0 +1,6 @@
+const OnBoarding = () => {
+    return (
+        <div>Welcome OnBoarding</div>
+    )
+}
+export default OnBoarding
